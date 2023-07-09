66e6916ac9280a01e22499578bb00023b4a61185
Bibin Thomas <43011950+bibinthomas007@users.noreply.github.com>
2023-07-09 23:37:54 +0530

    Create AnalogReadWithStethoscope.ino
    This code is supposed to sent stethoscope readings to the esp32

---
+++ AnalogReadWithStethoscope.ino
@@ -0,0 +1,11 @@
+const int STETHOSCOPE_PIN = 36;
+
+void setup() {
+   // put your setup code here, to run once:
+   Serial.begin(115200);
+   analogReadResolution(10); //default is 12. Can be set between 9-12.
+}
+void loop() {
+   
+   int STETHOSCOPE_Reading = analogRead(STETHOSCOPE_PIN);
+}
